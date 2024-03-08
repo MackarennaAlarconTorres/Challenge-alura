e9af41d8f995f1c529ee5d3c5583fad34ee1c32d
Mackarenna Alarcón <mack.alarcon92@gmail.com>
2024-03-08 11:14:33 -0300

    funcionalidad con javascript

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-console.log("Ejecutando el sistema")
+const campo_texto = document.querySelector("#cuadroTexto");
+const campo_mensaje = document.querySelector("#mensaje");
+
+const matriz_code = [
+    ["e","enter"],
+    ["i","imes"],
+    ["a","ai"],
+    ["o","ober"],
+    ["u","ufat"],
+];
+
+function btnencriptar() {
+    const texto = encriptar(campo_texto.value);
+    campo_mensaje.value = texto;
+    document.getElementById("defecto").style.visibility = "hidden";
+}
+
+function encriptar(fraseEncriptada) {
+    for (let i = 0; i < matriz_code.length; i++) {
+        if(fraseEncriptada.includes(matriz_code[i][0])){
+            fraseEncriptada = fraseEncriptada.replaceAll(
+                matriz_code[i][0],matriz_code[i][1]
+            )
+        }
+    }
+    return fraseEncriptada;
+}
+
+function btnDesencriptar() {
+    const texto = desencriptar(campo_texto.value);
+    campo_mensaje.value = texto;
+}
+
+function desencriptar(fraseEncriptada) {
+    for (let i = 0; i < matriz_code.length; i++) {
+        if(fraseEncriptada.includes(matriz_code[i][1])){
+            fraseEncriptada = fraseEncriptada.replaceAll(
+                matriz_code[i][1],
+                matriz_code[i][0]
+            )
+        }
+    }
+    return fraseEncriptada;
+}
